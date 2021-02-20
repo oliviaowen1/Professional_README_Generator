061cb5df448f2ade61d3a5246a07c1340256cde5
Olivia Owen <olivia.owen@hotmail.com>
2021-02-20 19:51:06 +0000

    Added switch statements

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (lincense) {
+    switch (license) {
+      // MIT
+      case "MIT":
+        return "[![license: MIT](https://img.shields.io/badge/License-MIT-blue.svg)]";
+      // Mozilla
+      case "Mozilla Public License 2.0":
+        return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)]";
+      // ISC
+      case "ISC":
+        return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+      default: "";
+        break;
+    }
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (lincense) {
+    switch (license) {
+      // MIT
+      case "MIT":
+        return "This license is provided by MIT, found out more about the license here: (https://opensource.org/licenses/MIT)";
+      // Mozilla
+      case "Mozilla Public License 2.0":
+        return "This license is provided by Mozilla, found out more about the license here: (https://opensource.org/licenses/MPL-2.0)";
+      // ISC
+      case "ISC":
+        return "This license is provided by ISC, found out more about the license here :(https://opensource.org/licenses/ISC)";
+      default: "";
+        break;
+    }
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
