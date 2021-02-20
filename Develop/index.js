4a7386fa854e3a49b42053597639c66f93ed4fa8
Olivia Owen <olivia.owen@hotmail.com>
2021-02-20 19:24:56 +0000

    Rearranged questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,13 +7,13 @@ const generateMarkdown = require('./utils/generateMarkdown.js');
 const questions = [
     "What is your GitHub username?",
     "What is your email address?",
+    "Please select the correct license for your project",
     "What is the title of your project?",
     "Enter a short description of your project",
     "How do users install your project?",
     "How do users use your project?",
     "How can people contribute to this project?",
     "How can you test your project?",
-    "Please select the correct license for your project",
     "What's the link to your GitHub profile?" ];
 
 // TODO: Create a function to write README file
