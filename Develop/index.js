1d48f75bac141cabb42b27ed4ce3bc0f1c565eb2
Olivia Owen <olivia.owen@hotmail.com>
2021-02-20 19:15:42 +0000

    Added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,27 @@
 // TODO: Include packages needed for this application
 const inquirer = requre('inquirer');
-const fs = require ('fs');
-const generateMarkdown = require ('./utils/generateMarkdown.js');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is your GitHub username?",
+    "What is your email address?",
+    "What is the title of your project?",
+    "Enter a short description of your project",
+    "How do users install your project?",
+    "How do users use your project?",
+    "How can people contribute to this project?",
+    "How can you test your project?",
+    "Please select the correct license for your project",
+    "What's the link to your GitHub profile?"
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
